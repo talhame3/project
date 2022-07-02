b006ef801fab87c0e3b514fbff3d06adb2ccc1ab
talhame3 <talhame3@gmail.com>
2022-07-02 18:26:39 -0400

    updated project

--- src/main/java/com/hexaware/spring/model/Claim.java
+++ src/main/java/com/hexaware/spring/model/Claim.java
@@ -7,7 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import org.springframework.lang.NonNull;
+
 
 @Entity
 @Table(name="claims")
