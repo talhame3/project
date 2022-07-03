123bf5eb5a09fd71980058c09682f86c072569cd
talhame3 <talhame3@gmail.com>
2022-07-03 11:48:09 -0400

    little update

--- src/main/java/com/hexaware/spring/controller/FileController.java
+++ src/main/java/com/hexaware/spring/controller/FileController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -22,7 +23,7 @@ import com.hexaware.spring.model.FileDB;
 import com.hexaware.spring.service.FileStorageService;
 
 @Controller
-@CrossOrigin("http://localhost:8081")
+
 public class FileController {
 	
 	 @Autowired
